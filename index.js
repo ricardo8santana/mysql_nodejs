02275cfabe50a5999db92165b0c9864213d32c99
ricardo8santana <luisriccardo@proton.me>
2025-08-25 17:05:43 -0300

    conexao com banco de dados

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
+var conexao = require('./conexaoBanco');
 var express = require('express');
 var app = express();
+
+var bodyParser = require('body-parser'); 
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/cadastro.html");
 
 });
 
-app.listen(7000)
+app.post('/', function(req, res){
+    var nomecompleto = req.body.nomecompleto;
+    var email = req.body.email;
+    var senha = req.body.senha;  
+    
+    conexao.connect(function(error){
+    if(error) throw error;
 
+    //prevenindo sql injection
+   var sql = "INSERT INTO estudante(nomecompleto, email, senha) VALUES (?,?,?)";
+   conexao.query(sql, [nomecompleto, email, senha], function(error, result){
+    if(error) throw error;
+    res.send("Estudante cadastrado com sucesso!" + result.insertId);
+    
+   });
 
+});
+
+});
+
+app.listen(7000);
 
 /*
-var conexao = require('./conexaoBanco');
 
-conexao.connect(function(error){
-    if(error) throw error;
+
         // console.log("banco conectado com sucesso!");
 
-conexao.query("SELECT * FROM estudante", function(error, result){
-    if(error) throw error;
-    //console.log(result);
-    console.log([0]);
-    console.log(result[0].nomecompleto);
-});
-});
+
 */ 
